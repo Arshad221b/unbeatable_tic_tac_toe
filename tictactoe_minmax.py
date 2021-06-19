d248f12171f0101e9330ecbc9c54f68b0f807afb
arshad_221b <kaziarshad97@gmail.com>
2021-06-19 19:56:24 +0530

    added minmax part 1

--- tictactoe_minmax.py
+++ tictactoe_minmax.py
@@ -8,16 +8,16 @@ def winning_conditions(l):
 
     # if the row elemets are same 
     for i in range(len(l)):
-        if      l[i][0] == l[i][1] and l[i][1] == l[i][2] and l[i][0] == 'x':
+        if   l[i][0] == l[i][1] and l[i][1] == l[i][2] and l[i][0] == 'x':
             return 'player'
-        elif    l[i][0] == l[i][1] and l[i][1] == l[i][2] and l[i][0] == '0':
+        elif l[i][0] == l[i][1] and l[i][1] == l[i][2] and l[i][0] == '0':
             return 'comp'
     
     # if the colun elements are same
     for i in range(0,1):
-        if      l[0][i] == l[1][i] and l[1][i] == l[2][i] and l[0][i] == 'x':
+        if   l[0][i] == l[1][i] and l[1][i] == l[2][i] and l[0][i] == 'x':
             return 'player'
-        elif    l[0][i] == l[1][i] and l[1][i] == l[2][i] and l[0][i] == 'o':
+        elif l[0][i] == l[1][i] and l[1][i] == l[2][i] and l[0][i] == 'o':
             return 'comp'
 
     # check the digonal condition
@@ -31,6 +31,7 @@ def winning_conditions(l):
         return 'comp'
 
     return 'd'
+ 
 
 
 def print_grid(l):
@@ -56,16 +57,62 @@ def possible_moves(l):
                 blank_moves.append([i,j])
     return(blank_moves)
 
+scores = {
+    'player' : 10,
+    'comp' : -10,
+    'd' : 0
+}
+
+def minmax(l, depth, isMaximizing):
+    if winning_conditions(l) != 'null':
+        return(scores[winning_conditions(l)])
+
+    
+    # Human player
+    if isMaximizing:
+        bestscore = -100
+        blank_spaces = possible_moves(l)
+        for [x,y] in blank_spaces:
+            l[x][y] = 'x'
+            score = minmax(l, depth+1, False)
+            l[x][y] = '_'
+
+            bestscore = max(score, bestscore)
+        return(bestscore)
+
+    # AI player
+    else:
+        bestscore = 100
+        blank_spaces = possible_moves(l)
+        for [x,y] in blank_spaces:
+            l[x][y] = 'o'
+            score = minmax(l, depth+1, True)
+            l[x][y] = '_'
+            bestscore = min(score,bestscore)
+
+        return(bestscore)
 
 def computer_move(l):
     blank_moves = possible_moves(l)
     k = []
     if len(blank_moves)>0:
-        k = random.choice(blank_moves)
-
-    l[k[0]][k[1]] = 'o'
-    print("computer played its move!")
-    print_grid(l)
+        bestscore = 100
+        for [x,y] in blank_moves:
+            l[x][y]= 'o'
+            score = minmax(l, 0, False)
+            l[x][y]= '_'
+            if score <= bestscore:
+                print(x,y)
+                bestscore = score
+                print(bestscore)
+                k.append([x,y])
+        
+        move = random.choice(k)
+        l[move[0]][move[1]] = 'o'
+        print("computer played its move!")
+        print_grid(l)
+    else:
+        print(winning_conditions(l))
 
 
 def player_move():
